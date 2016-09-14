c03a20678bbea708cbabef6a0fdac3db42ace3a3
megagao <megagao@126.com>
2016-09-14 15:53:27 +0800

    修改了包名

---
+++ src/main/java/org/hqu/production_ms/dao/impl/SysUserDaoImpl.java
@@ -0,0 +1,21 @@
+package org.hqu.production_ms.dao.impl;
+
+import org.hqu.production_ms.dao.SysUserDao;
+import org.hqu.production_ms.domain.SysUser;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SysUserDaoImpl extends BaseDaoImpl<SysUser> implements SysUserDao{
+
+	/**
+	 * 设置命名空间
+	 */
+	public SysUserDaoImpl(){
+		this.setNs("org.hqu.telemedicine.mapper.SysUserMapper.");
+	}
+	@Override
+	public SysUser getUserByName(String username) {
+		return this.getSqlSession().selectOne(this.getNs() + "getUserByName", username);
+	}
+
+}
