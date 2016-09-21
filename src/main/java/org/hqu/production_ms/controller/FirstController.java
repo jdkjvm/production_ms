eaea765e8a86df0aca7ffd7fe4875abdc9fa1cdb
megagao <megagao@126.com>
2016-09-21 21:06:45 +0800

    修改了文件

--- src/main/java/org/hqu/production_ms/controller/FirstController.java
+++ src/main/java/org/hqu/production_ms/controller/FirstController.java
@@ -1,11 +1,8 @@
 package org.hqu.production_ms.controller;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.hqu.production_ms.domain.ActiveUser;
 
 
 @Controller
@@ -14,13 +11,6 @@ public class FirstController {
 	//系统首页
 	@RequestMapping("/first")
 	public String first(Model model)throws Exception{
-		
-		/*//从shiro的session中取activeUser
-		Subject subject = SecurityUtils.getSubject();
-		//取身份信息
-		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
-		//通过model传到页面
-		model.addAttribute("activeUser", activeUser);*/
 		
 		return "login.jsp";
 	}
